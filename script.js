a5d0074c10e4ac65508692498a0bebe647c3c3cb
Melanie221 <2110631250048@student.unsika.ac.id>
2023-06-21 22:08:07 +0700

    web

---
+++ script.js
@@ -0,0 +1,18 @@
+// Toggle class active
+const menu = document.querySelector ('.menu');
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+    menu.classList.toggle('active');
+};
+
+// NAVBAR I
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector(".container-navbar").style.top = "0";
+    } else {
+    document.querySelector(".container-navbar").style.top = "-150px";
+    }
+    prevScrollpos = currentScrollPos;
+};
